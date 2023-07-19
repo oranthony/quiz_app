f41d8fcb3f2303a96aeee5991e789ca847db4774
Anthony Loroscio <anthonyloroscio@gmail.com>
2023-07-19 13:20:22 +0200

    Add link to Privacy Policy to comply to Google Play Store rules

--- lib/src/view/screens/home_screen.dart
+++ lib/src/view/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quiz_app/models/singletons/token_singleton.dart';
 import 'package:quiz_app/utils/token_utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +14,17 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> with TokenHandler {
   bool _isTokenLoaded = false;
+
+  Future<void> showPrivacyPolicy() async {
+    String url = "http://anthonyloroscio.fr/cocktailize-privacy.html";
+    var urllaunchable =
+        await canLaunch(url); //canLaunch is from url_launcher package
+    if (urllaunchable) {
+      await launch(url); //launch is from url_launcher package to launch URL
+    } else {
+      print("URL can't be launched.");
+    }
+  }
 
   @override
   void initState() {
@@ -56,27 +68,41 @@ class _HomeScreenState extends State<HomeScreen> with TokenHandler {
             AnimatedContainer(
                 duration: const Duration(milliseconds: 350),
                 margin: const EdgeInsets.only(bottom: 50),
-                child: TextButton(
-                    style: ButtonStyle(
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.symmetric(
-                          horizontal: 40,
-                          vertical: 15,
-                        )),
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            _isTokenLoaded ? Colors.blue : Colors.grey),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                child: Column(
+                  children: <Widget>[
+                    TextButton(
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                                const EdgeInsets.symmetric(
+                              horizontal: 40,
+                              vertical: 15,
+                            )),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                _isTokenLoaded ? Colors.blue : Colors.grey),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
                                 RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(40.0),
                                     side:
                                         const BorderSide(color: Colors.blue)))),
-                    onPressed: () =>
-                        _isTokenLoaded ? context.push('/category') : null,
-                    child: Text("Start".toUpperCase(),
-                        style: const TextStyle(fontSize: 24))))
+                        onPressed: () =>
+                            _isTokenLoaded ? context.push('/category') : null,
+                        child: Text("Start".toUpperCase(),
+                            style: const TextStyle(fontSize: 24))),
+                    TextButton(
+                      onPressed: () => showPrivacyPolicy(),
+                      child: const Text(
+                        "Privacy Policy",
+                        style: TextStyle(
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    )
+                  ],
+                ))
           ]),
     ));
   }
